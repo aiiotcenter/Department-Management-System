747d35b47f2cbc6af07aeccada78d05b4e97b71f
Abdallah Ahmed <a.ahmed98@outlook.com>
2025-06-19 20:35:39 +0300

    fixed app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,9 @@
-<<<<<<< Updated upstream
-import StudentProfile from './pages/StudentProfile';
-=======
 import HomePage from './pages/HomePage';
->>>>>>> Stashed changes
 
 function App() {
     return (
         <div>
-<<<<<<< Updated upstream
-            <StudentProfile />
-=======
             <HomePage />
->>>>>>> Stashed changes
         </div>
     );
 }
